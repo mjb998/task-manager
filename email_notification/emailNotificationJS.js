a166b189bbc343656a5ff7a92140099ff1bf4bed
Jeremy Gonzalez <jpg9tb@mail.missouri.edu>
2017-11-06 11:40:39 -0600

    Learning Angular to do email notifications

--- email_notification/emailNotificationJS.js
+++ email_notification/emailNotificationJS.js
@@ -2,8 +2,9 @@ function EmailController($scope) {
 	$scope.isPopupVisible = false;
 	$scope.isComposePopupVisible = false;
 	$scope.composeEmail = {};
+	$scope.activeTab = "inbox";
 
-	$scope.showPopup = function () {
+	$scope.showPopup = function (email) {
 		$scope.isPopupVisible = true;
 		$scope.selectedEmail = email;
 	};
@@ -14,6 +15,7 @@ function EmailController($scope) {
 
 	$scope.showComposePopup = function () {
 		$scope.isComposePopupVisible = true;
+		$scope.composeEmail = {};
 	};
 
 	$scope.closeComposePopup = function () {
@@ -21,29 +23,39 @@ function EmailController($scope) {
 	}
 
 	$scope.sendEmail = function () {
-		alert($scope.composeEmail.to
+		$scope.isComposePopupVisible = false;
+		/*alert($scope.composeEmail.to
 			+ " " + $scope.composeEmail.subject
-			+ " " + $scope.composeEmail.body);
+			+ " " + $scope.composeEmail.body);*/
+
+			$scope.sentEmails.push($scope.composeEmail);
+			$scope.composeEmail.date = new Date();
+			$scope.composeEmail.from = "Me";
 	};
 
 	$scope.emails = [
 		{
 			from: 'John',
+			to: "Me",
 			subject: 'I love angular',
 			date: 'Jan 1',
 			body: 'hello world!'
 		},
 		{
 			from: 'Jack',
+			to: "Me",
 			subject: 'Angular and I are just friends',
 			date: 'Feb 15',
 			body: 'just kidding'
 		},
 		{
 			from: 'Ember',
+			to: "Me",
 			subject: 'I hate you Angular!',
 			date: 'Dec 8',
 			body: 'wassup dude'
 		}
 	];
+
+	$scope.sentEmails = [];
 }
